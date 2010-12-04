ceef53fd1cc0fb6629b7bb385d001c2f19321498
JV Clemence <james@jvc26.org>
2010-12-04 13:11:03 +0000

    Added gitmodules etc. for testing

--- autoinstall.sh
+++ autoinstall.sh
@@ -2,3 +2,22 @@
 
 # Script to autoinstall and configure settings for vim, bash etc.
 
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+git clone git://github.com/jvc26/dotfiles.git
+
+cd dotfiles
+
+git submodule init
+git submodule update
+
+cd ~
+ln -s "dotfiles/.vimrc" .vimrc
+#ln -s "dotfiles/.bashrc" .bashrc
