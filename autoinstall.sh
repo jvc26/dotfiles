392345998dcbf9ee463ab376bbe0b83336a6eb6b
James Verity Clemence <james@jvc26.org>
2010-12-20 11:42:31 +0000

    Fixed autoinstal script

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,21 +1,27 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Script to autoinstall and configure settings for vim, bash etc.
 
 if [ -d "$HOME/.vim" ] ; then
-    echo "Replacing .vim"
+    echo "Removing .vim"
     rm -rf $HOME/.vim
+    echo "Replacing .vim/"
+    ln -s $HOME/dotfiles/vim $HOME/.vim
 fi
 
-if [ -h "$HOME/.vimrc" ] ; then
-    echo "Replacing .vimrc"
+if [ -f "$HOME/.vimrc" ] ; then
+    echo "Removing .vimrc"
     rm -rf $HOME/.vimrc
+    echo "Replacing .vimrc"
+    ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+fi
+
+if [ -f "$HOME/.tmux.conf" ] ; then
+    echo "Removing .tmux.conf"
+    rm -rf $HOME/.tmux.conf
+    echo "Replacing .tmux.conf"
+    ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 fi
 
 git submodule init
 git submodule update
-
-cd $HOME
-ln -s "$HOME/dotfiles/.vimrc" $HOME/.vimrc
-ln -s "$HOME/dotfiles/vim" $HOME/.vim
-#ln -s "dotfiles/.bashrc" .bashrc
