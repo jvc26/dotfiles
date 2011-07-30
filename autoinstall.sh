659dde1b5e5f9cb770d2edf6a136b5c2bdbf018e
JV Clemence <james@jvc26.org>
2011-07-30 14:47:27 +0100

    Updated autoinstall to handle first install

--- autoinstall.sh
+++ autoinstall.sh
@@ -2,38 +2,43 @@
 
 # Script to autoinstall and configure settings for vim, bash etc.
 
-if [ -d "$HOME/.vim" ] ; then
-    echo "Removing .vim"
-    rm -rf $HOME/.vim
+if [ -d "$HOME/.vim" ] 
+then
     echo "Replacing .vim/"
+    rm -rf $HOME/.vim
+    ln -s $HOME/dotfiles/vim $HOME/.vim
+else
+    echo "Inserting .vim/"
     ln -s $HOME/dotfiles/vim $HOME/.vim
 fi
 
-if [ -f "$HOME/.vimrc" ] ; then
-    echo "Removing .vimrc"
-    rm -rf $HOME/.vimrc
+if [ -f "$HOME/.vimrc" ]
+then
     echo "Replacing .vimrc"
+    rm -rf $HOME/.vimrc
+    ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+else
+    echo "Inserting .vimrc"
     ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 fi
 
-if [ -f "$HOME/.tmux.conf" ] ; then
-    echo "Removing .tmux.conf"
-    rm -rf $HOME/.tmux.conf
+if [ -f "$HOME/.tmux.conf" ]
+then
     echo "Replacing .tmux.conf"
+    rm -rf $HOME/.tmux.conf
+    ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+else
+    echo "Inserting .tmux.conf"
     ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 fi
 
-if [ -f "$HOME/.muttrc" ] ;  then
-    echo "Removing .muttrc"
-    rm -rf $HOME/.muttrc
-    echo "Replacing .muttrc"
-    ln -s $HOME/dotfiles/.muttrc $HOME/.muttrc
-fi
-
-if [ -f "$HOME/.bashrc" ] ; then
-    echo "Removing .bashrc"
-    rm -rf $HOME/.bashrc
+if [ -f "$HOME/.bashrc" ]
+then
     echo "Replacing .bashrc"
+    rm -rf $HOME/.bashrc
+    ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
+else
+    echo "Inserting .bashrc"
     ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
 fi
 
